50e7400279f281df2f5e065f4a61ff1f04dde224
Damon Swatton on ENVY <damon.swatton@gmail.com>
2021-09-03 13:52:44 +0100

    Add _creatRoute class tobutton.dart in prep for implementing a FAB-activated page transition.

--- lib/button.dart
+++ lib/button.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 // Import project-specific files.
 import 'package:dash_cam_app/app_settings.dart';
+import 'package:dash_cam_app/base_page.dart';
 
 class Button extends StatelessWidget {
   const Button({
@@ -30,4 +31,33 @@ class Button extends StatelessWidget {
       ),
     );
   }
+}
+
+//  Implement PageRouteBuilder method for managing page/route transitions
+//  that result from FAB activation.
+Route _createRoute(String title) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) =>
+        BasePage(
+          title: title,
+        ),
+    transitionDuration: const Duration(seconds: 2),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(
+        begin: begin,
+        end: end,
+      ).chain(CurveTween(
+        curve: curve,
+      ));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
