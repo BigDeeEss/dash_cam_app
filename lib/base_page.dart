2d5d5dec044f49eb7f4d97b20e171f5644681743
Damon Swatton on ENVY <damon.swatton@gmail.com>
2021-09-03 11:22:37 +0100

    Add a bottomNavigationBar widget to BasePage.
    Add app_settings.dart.
    
    The bottomNavigationBar height is calculated using a scale factor applied to the appBar height. In order to be able to extract the appBar height from the BuildContext, a Builder class is used which delegates its build to a callback. This opens the parent widget's BuildContext for interrogating and getting the appBar height.

--- lib/base_page.dart
+++ lib/base_page.dart
@@ -1,6 +1,10 @@
 //  Import flutter packages.
 import 'package:flutter/material.dart';
 
+// Import project-specific files.
+import 'package:dash_cam_app/app_settings.dart' as appSettings;
+
+/// [BasePage] implements a basic page layout design.
 class BasePage extends StatefulWidget {
   const BasePage({
     required this.title,
@@ -21,6 +25,32 @@ class _BasePageState extends State<BasePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+
+      //  Use Builder(...) widget because it is not possible to get the appBar
+      //  height from the current BuildContext when it doesn't yet include the
+      //  Scaffold(...) class being returned by this widget.
+      bottomNavigationBar: Builder(
+        builder: (BuildContext context) {
+          //  [height] variable for storing appBar height.
+          double height = MediaQuery.of(context).padding.top + kToolbarHeight;
+
+          return BottomAppBar(
+            color: Colors.blue,
+            child: Row(
+              children: <Widget>[
+                SizedBox(
+                  //  Set height of the BottomAppBar(...) class variable using
+                  //  SizedBox(...). Get height from [context] by first
+                  //  extracting the immediate Scaffold(...), and then getting
+                  //  the value for appBarMaxHeight. Need to take into account
+                  //  appBarMaxHeight possibly being null.
+                  height: height * appSettings.appBarHeightScaleFactor,
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
