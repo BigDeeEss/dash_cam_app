155c86c07531ff7ce3571694232f6e63bd933823
Damon Swatton on PAVILION <damon.swatton@gmail.com>
2021-10-07 21:40:54 +0100

    Add app settings page.
    A simple settings page implemented using settings_ui.dart.

--- lib/settings_page_contents.dart
+++ lib/settings_page_contents.dart
@@ -1,11 +1,87 @@
 //  Import flutter packages.
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
-class SettingsPageContents extends StatelessWidget {
+class SettingsPageContents extends StatefulWidget {
   const SettingsPageContents({Key? key}) : super(key: key);
 
   @override
+  State<SettingsPageContents> createState() => _SettingsPageContentsState();
+}
+
+class _SettingsPageContentsState extends State<SettingsPageContents> {
+  @override
   Widget build(BuildContext context) {
+    bool isSwitched = false;
+
+    return Padding(
+      padding: EdgeInsetsDirectional.only(
+        start: 5,
+        top: 5,
+        end: 100,
+        bottom: 5,
+      ),
+      child: SettingsList(
+        sections: [
+          SettingsSection(
+            titlePadding: EdgeInsets.all(20),
+            title: 'Section 1',
+            tiles: [
+              SettingsTile(
+                title: 'Language',
+                subtitle: 'English',
+                leading: Icon(Icons.language),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.switchTile(
+                title: 'Use System Theme',
+                leading: Icon(Icons.phone_android),
+                switchValue: isSwitched,
+                onToggle: (value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                },
+              ),
+            ],
+          ),
+          SettingsSection(
+            titlePadding: EdgeInsets.all(20),
+            title: 'Section 2',
+            tiles: [
+              SettingsTile(
+                title: 'Security',
+                subtitle: 'Fingerprint',
+                leading: Icon(Icons.lock),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.switchTile(
+                title: 'Use fingerprint',
+                leading: Icon(Icons.fingerprint),
+                switchValue: true,
+                onToggle: (value) {},
+              ),
+              SettingsTile(
+                title: 'Language',
+                subtitle: 'English',
+                leading: Icon(Icons.language),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.switchTile(
+                title: 'Use System Theme',
+                leading: Icon(Icons.phone_android),
+                switchValue: isSwitched,
+                onToggle: (value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
     return Center(
       child: Container(
         width: 600,
