8b6b4cf339240275aa20b31249957a4ba801bff2
BigDeeEss <damon.swatton@gmail.com>
2022-01-03 20:49:42 +0000

    Add settings_page_list_tile.dart containing prototype SettingsPageListTile<T> widget.

--- lib/settings_page_contents.dart
+++ lib/settings_page_contents.dart
@@ -3,27 +3,26 @@ import 'package:flutter/material.dart';
 
 //  Import project-specific files.
 import 'package:dash_cam_app/notification_notifier.dart';
+import 'package:dash_cam_app/settings_page_list_tile.dart';
 
+/// Implement a settings page based on a bespoke list view.
 class SettingsPageContents extends StatelessWidget {
   SettingsPageContents({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //  Insert an instance of NotificationNotifier above
+    //  _SettingsPageContentsList in order to trigger rebuild of list tiles.
     return NotificationNotifier<ScrollNotification, ScrollUpdateNotification>(
       child: _SettingsPageContentsList(),
     );
   }
 }
 
-class _SettingsPageContentsList extends StatefulWidget {
-  _SettingsPageContentsList({Key? key}) : super(key: key);
+class _SettingsPageContentsList extends StatelessWidget {
+  const _SettingsPageContentsList({Key? key}) : super(key: key);
 
-  @override
-  State<_SettingsPageContentsList> createState() => __SettingsPageContentsListState();
-}
-
-class __SettingsPageContentsListState extends State<_SettingsPageContentsList> {
-  List<Color> colors = [
+  final List<Color> colors = const [
     Colors.blueGrey,
     Colors.green,
     Colors.deepOrange,
@@ -58,11 +57,11 @@ class __SettingsPageContentsListState extends State<_SettingsPageContentsList> {
           alignment: Alignment.center,
           color: colors[0],
         ),
-        ValueListenableBuilder<dynamic>(
+        ValueListenableBuilder<double>(
           valueListenable: NotificationNotifier.of(context).notificationData,
-          builder: (BuildContext context, dynamic value, __,){
+          builder: (BuildContext context, double value, __,){
             return Container(
-              height: value.toDouble() % 290 + 10,
+              height: value % 290 + 10,
               width: 50,
               alignment: Alignment.center,
               color: colors[3],
@@ -177,11 +176,11 @@ class __SettingsPageContentsListState extends State<_SettingsPageContentsList> {
           alignment: Alignment.center,
           color: colors[0],
         ),
-        ValueListenableBuilder<dynamic>(
+        ValueListenableBuilder<double>(
           valueListenable: NotificationNotifier.of(context).notificationData,
-          builder: (BuildContext context, dynamic value, __,){
+          builder: (BuildContext context, double value, __,){
             return Container(
-              height: value.toDouble() % 290 + 10,
+              height: value % 290 + 10,
               width: 50,
               alignment: Alignment.center,
               color: colors[3],
