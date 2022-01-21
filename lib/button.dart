00b2b4aecb190be7bec8a798b3a4e065fa32469a
BigDeeEss <damon.swatton@gmail.com>
2022-01-21 19:00:09 +0000

    Implement RectGetter method for getting widget location. on screen.
    RectGetter screen coordinates agree with GlobalKeyExtension method.

--- lib/button.dart
+++ lib/button.dart
@@ -1,5 +1,6 @@
 //  Import flutter packages.
 import 'package:flutter/material.dart';
+import 'package:rect_getter/rect_getter.dart';
 
 // Import project-specific files.
 import 'package:dash_cam_app/app_settings.dart';
@@ -18,13 +19,49 @@ class Button extends StatelessWidget {
   final ButtonSpec buttonSpec;
 
   final GlobalKey globalKey = GlobalKey();
+  final GlobalKey<RectGetterState> rectGetterKey = RectGetter.createGlobalKey();
 
   @override
   Widget build(BuildContext context) {
     WidgetsBinding.instance!.addPostFrameCallback((_) {
-      print('absolute coordinates on screen: ${globalKey.globalPaintBounds}');
+      print('GlobalKeyExtension: absolute coordinates on screen: ${globalKey.globalPaintBounds}');
     });
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      print('RectGetter: absolute coordinates on screen: ${RectGetter.getRectFromKey(rectGetterKey)}');
+    });
+
     //  An IconButton with a circular background and geometry-dependent padding.
+    return RectGetter(
+      key: rectGetterKey,
+      child: Container(
+        key: globalKey,
+        decoration: BoxDecoration(
+          border: AppSettings.drawLayoutBounds
+              ? Border.all(width: 0.0, color: Colors.redAccent) : null,
+        ),
+        child: Padding(
+          padding: (AppSettings.buttonAlignment.y < 0)
+              ? AppSettings.buttonPaddingDown
+              : AppSettings.buttonPaddingUp,
+          child: Container(
+            decoration: BoxDecoration(
+              border: AppSettings.drawLayoutBounds
+                  ? Border.all(width: 0.0, color: Colors.redAccent) : null,
+            ),
+            child:CircleAvatar(
+              radius: this.buttonSpec.size / 2.0,
+              backgroundColor: Colors.lightBlue,
+              child: IconButton(
+                icon: this.buttonSpec.icon,
+                color: Colors.white,
+                onPressed: () => this.buttonSpec.onPressed(context),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
     return Container(
       key: globalKey,
       decoration: BoxDecoration(
